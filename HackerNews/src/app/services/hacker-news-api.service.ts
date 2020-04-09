ab64a524edd32c5b0b87fd9ab68004888c8c5cd6
Orla <orlaith.coleman@gmail.com>
2020-04-09 21:29:28 +1000

    add API calls to connection-service.ts

--- HackerNews/src/app/services/hacker-news-api.service.ts
+++ HackerNews/src/app/services/hacker-news-api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Story } from '../models/hacker-news-model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HackerNewsApiService {
 
-  constructor() { }
+  id: number;
+
+  constructor(private http: HttpClient) { }
+
+  top_stories_url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
+  individual_story_url = `https://hacker-news.firebaseio.com/v0/item/${this.id}.json?print=pretty`
+
+  getTopStories(){
+    return this.http.get(this.top_stories_url);
+  }
+  getIndividualStory(){
+    return this.http.get<Story>(this.individual_story_url);
+  }
 }
