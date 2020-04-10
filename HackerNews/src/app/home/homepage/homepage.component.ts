bb4abde3dc9f6cd86ddcef5f967dd640d144a44c
Orla <orlaith.coleman@gmail.com>
2020-04-10 01:03:04 +0100

    updated functions

--- HackerNews/src/app/home/homepage/homepage.component.ts
+++ HackerNews/src/app/home/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HackerNewsApiService } from 'src/app/services/hacker-news-api.service';
 import { Story } from 'src/app/models/hacker-news-model';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-homepage',
@@ -9,17 +10,20 @@ import { Story } from 'src/app/models/hacker-news-model';
 })
 export class HomepageComponent implements OnInit {
 
+  safeUrl: SafeResourceUrl;
+  evenUrl: SafeResourceUrl;
+  oddUrl: SafeResourceUrl;
+
   id_array: any;
 
-  stories_array: Story[] = [];
-  even_story_array: Story[]=[];
-  odd_story_array: Story[]=[];
+  even_story_array: Story[] = [];
+  odd_story_array: Story[] = [];
 
   tagline: string;
 
   now: Date = new Date();
 
-  constructor(private service: HackerNewsApiService) { 
+  constructor(private service: HackerNewsApiService, public sanitizer: DomSanitizer) {
     this.now = new Date();
   }
 
@@ -27,44 +31,60 @@ export class HomepageComponent implements OnInit {
     this.getStoryIDs();
 
     setTimeout(
-      function(){
-        this.getStories();
-      }.bind(this), 2000);
+      function () {
+        this.getStories(50);
+      }.bind(this), 5000);
 
   }
 
-  getStoryIDs(){
-    return this.service.getTopStoryIDs().subscribe( data => {
+  getStoryIDs() {
+    return this.service.getTopStoryIDs().subscribe(data => {
       this.id_array = data;
-      console.log(this.id_array)
-      console.log("done")
-        });
-        
-  }
-
-  getStories(){
-    //itterate through array of ID's & for each one make GET request for story data, then add each one to an array
-    this.id_array.forEach(id => {
-      this.service.getIndividualStory(id).subscribe( data => {
-        this.stories_array.push(data);
-        });
     });
+
   }
 
-  calcTime(){
-    //calc time from time given through API call 
-    this.stories_array.forEach( element => {
+  calcEvenTime(){
+    this.even_story_array.forEach( element => {
       let unixtimestamp = element.time;
-      let dateNow = new Date(unixtimestamp*1000);
-      console.log(((this.now.getHours()) - dateNow.getHours()));
+      let dateNow = new Date(unixtimestamp * 1000);
+
+      console.log( (this.now.getHours() - dateNow.getHours()) + " hours");
     })
-
   }
 
-  splitArrays(){
-    for( let i=1; i<this.stories_array.length; i++){
-      console.log(i);
+
+  getStories(num: number) {
+    this.even_story_array.length = 0;
+    this.odd_story_array.length = 0;
+
+    this.service.getIndividualStory(this.id_array[0]).subscribe(data => {
+      this.even_story_array.push(data);
+
+      this.tagline = (this.even_story_array[0].score + " points by " + this.even_story_array[0].by);
+
+      if (this.even_story_array[0].url == undefined) {
+        this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/img/generic_news2.jpg');
+      }
+      else {
+        this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.even_story_array[0].url);
+      }
+    });
+
+
+    for (let i = 1; i < num; i += 2) {
+      this.service.getIndividualStory(this.id_array[(i + 2)]).subscribe(data => {
+        this.even_story_array.push(data);
+        // this.evenUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/img/generic_news.jpg');
+      });
+      this.service.getIndividualStory(this.id_array[(i + 1)]).subscribe(data => {
+        this.odd_story_array.push(data);
+        //   this.oddUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/img/generic_news2.jpg');
+      });
     }
+
   }
+
+
 
 }
